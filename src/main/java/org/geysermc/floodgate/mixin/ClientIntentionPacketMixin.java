b1f214d9c02fd2fd3f796220cc04e5d087892bbf
onebeastchris <github@onechris.mozmail.com>
2023-02-10 14:51:36 +0100

    FabricProxyLite support - apply workaround of changing the limit of 255 to instead the Short.MAX_VALUE int, as also done by Paper/Spigot.

--- src/main/java/org/geysermc/floodgate/mixin/ClientIntentionPacketMixin.java
+++ src/main/java/org/geysermc/floodgate/mixin/ClientIntentionPacketMixin.java
@@ -1,14 +1,19 @@
 package org.geysermc.floodgate.mixin;
 
 import net.minecraft.network.protocol.handshake.ClientIntentionPacket;
+import org.geysermc.floodgate.util.ProxyUtils;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(ClientIntentionPacket.class)
-public interface ClientIntentionPacketMixin {
-
-    @Accessor("hostName")
-    @Mutable
-    void setAddress(String address);
+public class ClientIntentionPacketMixin {
+    @ModifyConstant(method = "<init>*", constant = @Constant(intValue = 255))
+    private int floodgate$modifyMaxCapacity(int defaultValue) {
+        if (ProxyUtils.isProxyData()) {
+            //let's not modify if unnecessary
+            return Short.MAX_VALUE;
+        }
+        return defaultValue;
+    }
 }

84b51c71a51a8e56712a7dadf49b203225031788
onebeastchris <github@onechris.mozmail.com>
2023-02-11 01:46:56 +0100

    use a MixinConfigPlugin to check whether we need to load hacky mixin; "proper" mixin application

--- src/main/java/org/geysermc/floodgate/mixin/ClientIntentionPacketMixin.java
+++ src/main/java/org/geysermc/floodgate/mixin/ClientIntentionPacketMixin.java
@@ -1,19 +1,14 @@
 package org.geysermc.floodgate.mixin;
 
 import net.minecraft.network.protocol.handshake.ClientIntentionPacket;
-import org.geysermc.floodgate.util.ProxyUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(ClientIntentionPacket.class)
 public class ClientIntentionPacketMixin {
-    @ModifyConstant(method = "<init>*", constant = @Constant(intValue = 255))
+    @ModifyConstant(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", constant = @Constant(intValue = 255))
     private int floodgate$modifyMaxCapacity(int defaultValue) {
-        if (ProxyUtils.isProxyData()) {
-            //let's not modify if unnecessary
             return Short.MAX_VALUE;
-        }
-        return defaultValue;
     }
 }
--- src/main/java/org/geysermc/floodgate/util/ProxyUtils.java
+++
@@ -1,9 +0,0 @@
-package org.geysermc.floodgate.util;
-
-import net.fabricmc.loader.api.FabricLoader;
-
-public class ProxyUtils {
-    public static boolean isProxyData() {
-        return FabricLoader.getInstance().isModLoaded("fabricproxy-lite");
-    }
-}

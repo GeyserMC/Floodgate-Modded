15f4ec90011693ca91610b47fee0709983629a32
onebeastchris <github@onechris.mozmail.com>
2024-04-27 22:56:48 +0200

    some minor stuff

--- src/main/java/org/geysermc/floodgate/inject/fabric/FabricInjector.java
+++ src/main/java/org/geysermc/floodgate/inject/fabric/FabricInjector.java
@@ -3,6 +3,7 @@ package org.geysermc.floodgate.inject.fabric;
 import io.netty.channel.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.geysermc.floodgate.inject.CommonPlatformInjector;
 
 @RequiredArgsConstructor
@@ -19,16 +20,16 @@ public final class FabricInjector extends CommonPlatformInjector {
     public void injectClient(ChannelFuture future) {
         future.channel().pipeline().addFirst("floodgate-init", new ChannelInboundHandlerAdapter() {
             @Override
-            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            public void channelRead(@NonNull ChannelHandlerContext ctx, @NonNull Object msg) throws Exception {
                 super.channelRead(ctx, msg);
 
                 if (!(msg instanceof Channel channel)) {
                     return;
                 }
 
-                channel.pipeline().addLast(new ChannelInitializer<Channel>() {
+                channel.pipeline().addLast(new ChannelInitializer<>() {
                     @Override
-                    protected void initChannel(Channel channel) {
+                    protected void initChannel(@NonNull Channel channel) {
                         injectAddonsCall(channel, false);
                         addInjectedClient(channel);
                         channel.closeFuture().addListener(listener -> {
